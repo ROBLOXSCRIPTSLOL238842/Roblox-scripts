4895264881571c32c283e8722d58f98fc88efd3d
ROBLOXSCRIPTSLOL238842 <voloyuroz@mail.ru>
2024-06-30 20:35:03 +0300

    Create Ice-Hub-(New-Link).lua

---
+++ Ice-Hub-(New-Link).lua
@@ -0,0 +1,9 @@
+if isfolder("IceHub") then
+	delfolder("IceHub")
+end
+
+if game.PlaceId == 920587237 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/ReQiuYTPL/hub/main/ReQiuYTPLHub.lua"))()
+elseif game.PlaceId == 4924922222 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/REDzHUB/REDzHUB/main/REDzHUB"))()
+end
